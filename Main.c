1480454f65f307778200e7f47281ecba6a4388fc
Ben Echard <38450709+benmechard@users.noreply.github.com>
2018-04-20 02:57:45 -0400

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,23 +1,20 @@
-#pragma config(Sensor, dgtl9,  					pneumatics,    sensorDigitalOut)
-#pragma config(Motor,  port2,           FL,            tmotorVex393_MC29, openLoop, reversed)
-#pragma config(Motor,  port3,           FR,            tmotorVex393_MC29, openLoop)
-#pragma config(Motor,  port4,           BL,            tmotorVex393_MC29, openLoop, reversed)
-#pragma config(Motor,  port5,           BR,            tmotorVex393_MC29, openLoop)
-#pragma config(Motor,  port6,           LLL1,          tmotorVex393_MC29, openLoop)
-#pragma config(Motor,  port7,           LLL2,          tmotorVex393_MC29, openLoop, reversed)
-#pragma config(Motor,  port8,           ULL1,          tmotorVex393_MC29, openLoop)
-#pragma config(Motor,  port9,           ULL2,          tmotorVex393_MC29, openLoop)
+#pragma config(Sensor, dgtl9,  			       pneumatics, sensorDigitalOut)
+#pragma config(Motor,  port2,           FrontLeft,           tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port3,           FrontRight,          tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port4,           BackLeft,            tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port5,           BackRight,           tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port6,           LowerLift1,          tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port7,           LowerLift2,          tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port8,           UpperLift1,          tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port9,           UpperLift2,          tmotorVex393_MC29, openLoop)
 
 //programmer definitions
 #define X vexRT[Ch4]
 #define Y vexRT[Ch3]
-#define Z vexRT[Ch2]
-#define ZZ vexRT[Ch1]
+#define Z vexRT[Ch1]
 
-#define X2 vexRT[Ch4Xmtr2]
 #define Y2 vexRT[Ch3Xmtr2]
 #define Z2 vexRT[Ch2Xmtr2]
-#define ZZ2 vexRT[Ch1Xmtr2]
 
 #define liftFixL1 vexRT[Btn6UXmtr2]
 #define liftFixL2 vexRT[Btn6DXmtr2]
@@ -27,57 +24,58 @@
 #define pneumaticsButton vexRT[Btn8UXmtr2]
 
 //pre-defined variables
-int thresh = 15;
+int threshold = 15;
 int liftSpeed = 50;
 int timer = 0;
 
 //base mechanim movement
 task base(){
-	motor[FR] = Y + ZZ - X;
-	motor[BR] =  Y + ZZ + X;
-	motor[FL] = Y - ZZ + X;
-	motor[BL] =  Y - ZZ - X;
+	motor[FrontRight] = Y + Z - X;
+	motor[BackRight] =  Y + Z + X;
+	motor[FrontLeft] = Y - Z + X;
+	motor[BackLeft] =  Y - Z - X;
 }
 
-//lift/lower lift
+//lift movement
 task lift(){
-	if (Y2 > thresh){
-		motor[ULL1] = liftSpeed;
-		motor[ULL2] = liftSpeed;
+	if (Y2 > threshold){
+		motor[UpperLift1] = liftSpeed;
+		motor[UpperLift2] = liftSpeed;
 	}
-	else if (Z2 > thresh){
-		motor[LLL1] = liftSpeed*2;
-		motor[LLL2] = liftSpeed*2;
+	else if (Z2 > threshold){
+		motor[LowerLift1] = liftSpeed*2;
+		motor[LowerLift2] = liftSpeed*2;
 	}
-	else if(Y2 < -thresh){
-		motor[ULL1] = -liftSpeed;
-		motor[ULL2] = -liftSpeed;
+	else if(Y2 < -threshold){
+		motor[UpperLift1] = -liftSpeed;
+		motor[UpperLift2] = -liftSpeed;
 	}
-	else if(Z2 < -thresh){
-		motor[LLL1] = -liftSpeed*2;
-		motor[LLL2] = -liftSpeed*2;
+	else if(Z2 < -threshold){
+		motor[LowerLift1] = -liftSpeed*2;
+		motor[LowerLift2] = -liftSpeed*2;
 	}
-	else if(Z2 < thresh & Z2 > -thresh & Y2 < thresh & Y2 > -thresh){
-		motor[ULL1] = 0;
-		motor[ULL2] = 0;
-		motor[LLL1] = 0;
-		motor[LLL2] = 0;
+	else if(Z2 < threshold & Z2 > -threshold & Y2 < threshold & Y2 > -threshold){
+		motor[UpperLift1] = 0;
+		motor[UpperLift2] = 0;
+		motor[LowerLift1] = 0;
+		motor[LowerLift2] = 0;
 	}
-	if(liftFixL1){
-		motor[LLL1] = -liftSpeed;
+	//to correct and missaligned lifts individually
+	if(liftFixL1){ 
+		motor[LowerLift1] = -liftSpeed;
 	}
 	if(liftFixL2){
-		motor[LLL2] = -liftSpeed;
+		motor[LowerLift2] = -liftSpeed;
 	}
 	if(liftFixU1){
-		motor[ULL1] = -liftSpeed;
+		motor[UpperLift1] = -liftSpeed;
 	}
 	if(liftFixU2){
-		motor[ULL2] = -liftSpeed;
+		motor[UpperLift2] = -liftSpeed;
 	}
 }
 
-//open/close claw
+//claw controls
 task claw(){
 	if(pneumaticsButton & timer > 1000){
 		timer = 0;
@@ -104,5 +102,5 @@ task main(){
 /*
 Vex Robotics Competition Code
 Written By Ben Echard
-In 2016
+2016
 */
